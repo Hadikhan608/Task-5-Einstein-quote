6a51a5c20a3fbd6e7204fde9ea6df4a6b22de9e1
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 22:50:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//repeat task 4 but store famous person name in a variable called famous person, 
+//then compose your message and store it in new variable called message
+//print your message.
+var famousperson = "Eistein";
+var message = "A person never made a mistake never tried anything new";
+console.log("".concat(famousperson, ":").concat(message));
---
+++ main.ts
@@ -0,0 +1,6 @@
+//repeat task 4 but store famous person name in a variable called famous person, 
+//then compose your message and store it in new variable called message
+//print your message.
+let famousperson:string = "Eistein";
+let message:string ="A person never made a mistake never tried anything new";
+console.log(`${famousperson}:${message}`);
